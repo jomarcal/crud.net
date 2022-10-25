b7b50953d69c69d063811b9b5b88ff8c40554751
joseph <jcszjunior@gmail.com>
2022-10-25 15:53:23 -0300

    first commit

---
+++ AspCore_Crud/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspCore_Crud.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name = "Name")]
+        public string? Name {get; set; }
+
+    }
+}
